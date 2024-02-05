ecdba7fb31a90c011f7ef270d94a9ad290a4118c
Nikita <pocketkurt@gmail.com>
2024-02-05 21:48:46 +0300

    fix calculation with discount

--- app/routers/bill_router.py
+++ app/routers/bill_router.py
@@ -205,13 +205,13 @@ async def none_presse(query: CallbackQuery, callback_data: RightButton) -> None:
 async def print_overall(query: CallbackQuery, callback_data: CallbackData):
     await query.answer()
     user_name = query.from_user.username
-    price = bill[user_name] if callback_data.text == 'overall' else bill[user_name]/2
+    price = bill[user_name]["total"] if callback_data.text == 'overall' else bill[user_name]["total"]/2
     message_for_user = '\n'
     for pos in bill[user_name]:
         if pos == 'total':
             continue
         message_for_user += f'{pos}: {bill[user_name][pos]} RUB\n'
-    message_for_user += f'\n\n\n Итого: {bill[user_name]["total"]}'
+    message_for_user += f'\n\n\nИтого: {price}'
     await bot.send_message(query.message.chat.id, text=f'@{user_name}, ваш чек:\n{message_for_user}')
     bill.pop(user_name, '')
 
